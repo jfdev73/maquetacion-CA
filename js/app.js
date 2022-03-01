f5c8ed7105864cb6827bab64470cf514d72fab61
JUAN FERNANDO MIRANDA MONROY <becario.fernando@subsecretaria.dominio>
2022-03-01 17:10:18 -0600

    menu desplegable con flecha funcionando

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const opcion = document.querySelectorAll('.menu__item--show');
+const prueba = document.querySelectorAll('.prueba');
+const h = document.querySelector('.menu__nesting');
+const flecha = document.querySelector('.menu__arrow');
+console.log("esta es: ",h);
+console.log("opcion",{opcion});
+
+// Permite recorrer cada una de nuestras opciones
+opcion.forEach(e => {
+
+    // Añadimos un evento a cada elemento seleccionado
+    e.addEventListener('click', function(e){
+
+        // Alteranmos las clases de nuestros enlaces
+        const padre = e.target.parentNode;
+        console.log("padre:", padre);
+        padre.children[1].classList.toggle('hola');
+        console.log("segundo hijo", padre.children[1]);
+        padre.parentNode.children[1].classList.toggle('hola');
+        h.classList.toggle('view');
+        flecha.classList.toggle('menu__90')
+    });
+
+
+});
